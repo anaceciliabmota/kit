ea6067a7d4e215f26eb8b11efef9e3dd3164f5c8
anaceciliabmota <anaceciliabezerra10@gmail.com>
2023-12-02 12:25:06 -0300

    função de escolher3NosAleatorios alterada. função de Ordenar custo adicionada.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ typedef struct Solucao {
         vector<int> sequencia;
         double valorObj;
     } Solucao;
+
 struct InsertionInfo{
         int noInserido;
         int arestaRemovida;
@@ -19,15 +20,28 @@ struct InsertionInfo{
 vector<int> escolher3NosAleatorios(size_t vertices){
     vector<int> sequencia;
     srand(time(NULL));
-    sequencia.push_back(1);
-    sequencia.push_back(rand() % (vertices) + 1);
-    do{
-        sequencia.push_back(rand() % (vertices) + 1);
-    }while(sequencia[1] == sequencia[2]);
-    do{
-        sequencia.push_back(rand() % (vertices) + 1);
-    }while(sequencia[1] == sequencia[3] || sequencia[2] == sequencia[3]);
-    sequencia.push_back(1);
+    sequencia.insert(sequencia.begin(), (rand() % (vertices) + 1));
+    
+    sequencia.insert(sequencia.begin() + 1, (rand() % (vertices) + 1));
+    while(sequencia[0] == sequencia[1]){
+        sequencia.erase(sequencia.begin() + 1);
+        sequencia.insert(sequencia.begin() + 1, (rand() % (vertices) + 1));
+    }
+
+    sequencia.insert(sequencia.begin() + 2, (rand() % (vertices) + 1));
+    while(sequencia[1] == sequencia[2] || sequencia[2] == sequencia[0]){
+        sequencia.erase(sequencia.begin() + 2);
+        sequencia.insert(sequencia.begin() + 2, (rand() % (vertices) + 1));
+    }
+    
+    sequencia.insert(sequencia.begin() + 3, (rand() % (vertices) + 1));
+    while(sequencia[1] == sequencia[3] || sequencia[2] == sequencia[3] || sequencia[0] == sequencia[3]){
+        sequencia.erase(sequencia.begin() + 3);
+        sequencia.insert(sequencia.begin() + 3, (rand() % (vertices) + 1));
+    }
+
+    sequencia.insert(sequencia.end(), sequencia[0]);
+    
     return sequencia;
 }
 
@@ -56,7 +70,45 @@ vector<int> NosRestantes(Solucao *s, size_t vertices){
     return CL;
 }
 
-// vector<int> Calcular
+// ainda não usei costrutor ao declarar o vector
+vector<InsertionInfo> calcularCustoInsercao(Solucao& s, vector<int>& CL, Data& data){
+    vector<InsertionInfo> custoInsercao((s.sequencia.size() - 1) * CL.size());
+    int l = 0;
+
+    for(int a = 0; a < s.sequencia.size() - 1;a++){
+        int i = s.sequencia[a];
+        int j = s.sequencia[a + 1];
+        for (auto k : CL){
+            custoInsercao[l].custo = data.getDistance(i, k) + data.getDistance(k, j) - data.getDistance(i, j);
+            custoInsercao[l].arestaRemovida = a;
+            custoInsercao[l].noInserido = k;
+            l++;
+        }
+    }
+    return custoInsercao;
+}
+
+void OrdenarEmOrdemCrescente(vector<InsertionInfo>& custoInsercao){
+    InsertionInfo aux;
+        for(int i = 0; i < custoInsercao.size();i++){
+            for(int j = i + 1; j < custoInsercao.size();j++){
+                if(custoInsercao[i].custo > custoInsercao[j].custo){  
+                    aux.custo = custoInsercao[i].custo;
+                    aux.arestaRemovida = custoInsercao[i].arestaRemovida;
+                    aux.noInserido = custoInsercao[i].noInserido;
+
+                    custoInsercao[i].custo = custoInsercao[j].custo;
+                    custoInsercao[i].arestaRemovida = custoInsercao[j].arestaRemovida;
+                    custoInsercao[i].noInserido = custoInsercao[j].noInserido;
+
+                    custoInsercao[j].custo = aux.custo;
+                    custoInsercao[j].arestaRemovida = aux.arestaRemovida;
+                    custoInsercao[j].noInserido = aux.noInserido;
+                }
+        }
+    }
+}
+
 
 int main(int argc, char** argv) {
 
@@ -85,13 +137,19 @@ int main(int argc, char** argv) {
     s1.sequencia = escolher3NosAleatorios(n); 
     ExibirSolucao(& s1); 
     vector<int> CL = NosRestantes(&s1, n);
-    //para testar a funcao NosRestantes
-    for(int i = 0; i < CL.size();i++){
+    for(int i = 0; i < CL.size(); i++){
         cout << CL[i] << " ";
     }
-    return 0;
-}
-
-
+    printf("\n");
     
-   
+    vector<InsertionInfo> custoInsercao = calcularCustoInsercao(s1, CL, data);
+    for(int i = 0; i < custoInsercao.size(); i++){
+        cout << custoInsercao[i].custo << " ";
+    }
+    OrdenarEmOrdemCrescente(custoInsercao);
+    for(int i = 0; i < custoInsercao.size(); i++){
+        cout << custoInsercao[i].custo << " ";
+    }
+    return 0;
+    
+}
